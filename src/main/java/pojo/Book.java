a40de8fda1deb33c9816de25b7cb03a2c5f2660f
zhangizyang_glb <zhangziyang_glb>
2020-11-25 17:54:28 +0800

    来自hotfix

--- src/main/java/pojo/Book.java
+++ src/main/java/pojo/Book.java
@@ -1,9 +1,14 @@
 package pojo;
 
+import java.util.Date;
+
 public class Book {
 
     private int id;//01
 
     private String name;//02
+    private int age;//on hotfix
+    private Date birthday;
+
 
 }
